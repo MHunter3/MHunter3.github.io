8b92a0b3df894223049e503514ffb4c39cc4490c
Morgan  Hunter <31575902+MHunter3@users.noreply.github.com>
2020-02-10 22:18:52 -0800

    Updated JS File

---
+++ CIS128.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#main_header").css({"color": "blue", "font-size": "500%"});
+    $(".img").css("border","2px solid blue")
+    //resize #main_header and .img
+	
+
+});
+});
+
+ 
+
